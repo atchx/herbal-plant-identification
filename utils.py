bff15e7b57b45f29495fbcb2963278ac73473644
Anisa Masnauli  Siregar <anisamasnauli05@gmail.com>
2022-06-24 08:36:17 +0700

    first commit

---
+++ utils.py
@@ -0,0 +1,157 @@
+disease_dic = {
+    'Binahong': """
+        <div class="row gy-4">
+          <div class="col-lg-6 position-relative align-self-start order-lg-last order-first">
+            <img src="../static/img/binahong.jpg" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-6 content order-last  order-lg-first">
+            <h3>Binahong</h3>
+            <p>
+              Binahong mempunyai sifat antioksidan, antinyeri, antibakteri, dan antiinflamasi yang sangat penting untuk kesehatan tubuh. 
+              Manfaat ini karena adanya banyak kandungan senyawa di dalam binahong, sebut saja tanin, saponin, dan flavonoid. Punya kandungan 
+              yang begitu kaya, tidak heran binahong menjadi salah satu tanaman herbal yang banyak dicari untuk mengatasi banyak masalah kesehatan. 
+              Berikut ini, di antaranya: 
+            </p>
+            <h6>- Mengobati Asam Urat</h6>
+            <h6>- Membantu Meningkatkan Fungsi Ginjal </h6>
+            <h6>- Mengatasi Hipertensi dan Masalah Kesehatan Jantung</h6>
+            <h6>- Membantu Mencegah Diabetes</h6>
+            <h6>- Mengobati Maag</h6>
+          </div>
+        </div>
+  """,
+
+    'Cincau_Rambat': """
+        <div class="row gy-4">
+          <div class="col-lg-6 position-relative align-self-start order-lg-last order-first">
+            <img src="../static/img/cincau.jpg" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-6 content order-last  order-lg-first">
+            <h3>Cincau Rambat</h3>
+            <p>
+               Tanaman cincau rambat ini dikenal dibeberapa daerah di Indonesia dengan nama camcauh, juju, tarawalu, dan kepleng. Tanaman ini memiliki 
+               memiliki daun yang khas, berwarna hijau, lembut serta tanaman cincau dapat merambat pada tanaman lainnya seperti sirih. Sifat kimiawi yang 
+               sudah diketahui oleh para ahli dalam tanaman cincau rambat ini cukup banyak diantaranya karbohidrat yang dapat menyerap air, zat lemak dan 
+               alkaloid siklein, kardioplegikum, tentradine, dimetil tenradine, polifenol, saponoid serta flavonoida, oleh karena itu dari beberapa sifat 
+               kimiawi yang terdapat pada tanaman cincau rambat ini dapat berkhasiat bagi kesehatan seseorang. Rimpang dan daun 
+               cincau rambat dapat dimanfaatkan untuk mengobati beberapa penyakit sebagai berikut:
+            </p>
+            <h6>- Mengobati demam</h6>
+            <h6>- Mengobati radang lambung, tifus, dan penyakit usus</h6>
+            <h6>- Mengobati tekanan darah tinggi</h6>
+          </div>
+        </div>
+    """,
+
+    'Jambu': """
+        <div class="row gy-4">
+          <div class="col-lg-6 position-relative align-self-start order-lg-last order-first">
+            <img src="../static/img/jambu.jpg" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-6 content order-last  order-lg-first">
+            <h3>Jambu</h3>
+            <p>
+              Daun jambu biji memiliki kandungan nutrisi seperti vitamin C, antioksidan, flavonoid, serta sifatnya yang anti-inflamasi. Kini kamu pun 
+              dapat memperoleh manfaat daun jambu biji tersebut melalui teh atau kapsul. Nah, berikut ini manfaat yang akan kamu peroleh saat mengonsumsi
+               daun jambu biji:
+            </p>
+            <h6>- Mengatasi Demam Berdarah</h6>
+            <h6>- Mencegah Diare</h6>
+            <h6>- Mengobati Bronkitis</h6>
+            <h6>- Menurunkan Kolesterol</h6>
+            <h6>- Mengobati Sariawan dan Sakit Gigi</h6>
+            <h6>- Mencegah Kanker</h6>
+            <h6>- Mencegah Obesitas</h6>
+            <h6>- Mengobati Masalah pada Usus dan Lambung</h6>
+          </div>
+        </div>
+    """,
+
+    'Keji_Beling': """
+        <div class="row gy-4">
+          <div class="col-lg-6 position-relative align-self-start order-lg-last order-first">
+            <img src="../static/img/keji.jpg" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-6 content order-last  order-lg-first">
+            <h3>Keji Beling</h3>
+            <p>
+               Keji beling punya nama latin Strobilanthes crispus (S. crispus). Tanaman ini masih termasuk dalam keluarga tanaman Acanthaceae, dan masih bersaudara 
+               dengan tanaman sambiloto. Keci beling sebetulnya dari Madagaskar tapi peredaran budidayanya sudah merambah sampai Indonesia. Menurut penelitian dari 
+               Universiti Sains Malaysia di Kelantan, daun pecah beling mengandung kafein, vitamin C, vitamin B1, dan vitamin B2 hingga sederet zat gizi lain yang 
+               tentunya punya khasiat baik dan bermanfaat bagi kesehatan. Daun keji beling sudah banyak diolah dan dipasarkan sebagai obat herbal. Antara lain berupa 
+               bubuk puyer, kapsul berisi bubuk, dan produk yang dapat dicampurkan ke dalam teh atau kopi. Manfaat keji beling yang berpotensi untuk kesehatan:
+            </p>
+            <h6>- Obat diabetes alami</h6>
+            <h6>- Menyembuhkan luka diabetes</h6>
+            <h6>- Melawan radikal bebas</h6>
+          </div>
+        </div>
+    """,
+
+    'Lada_Hitam': """
+        <div class="row gy-4">
+          <div class="col-lg-6 position-relative align-self-start order-lg-last order-first">
+            <img src="../static/img/lada.jpg" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-6 content order-last  order-lg-first">
+            <h3>Lada Hitam</h3>
+            <p>
+               Daun tanaman lada berbentuk bulat telur, tetapi ujungnya meruncing. Pada belahan atas, daun berwarna hijau tua mengkilat, sedang yang bawah berwarna hijau pucat. 
+               Panjangnya bisa mencapai 12–18 cm dengan ukuran lebar 5–10 cm. Daun akan berukuran lebih panjang jika berada pada batang bagian atas, begitu sebaliknya. Biasanya 
+               kuncup daun lada terbungkus oleh kelopak (sisik), jika dia mengembang, maka berjatuhanlah kelopak tersebut. Selain itu, daun tanaman ini sifatnya kenyal dan bertangkai. 
+               Beragam Manfaat Lada Hitam untuk Kesehatan:
+            </p>
+            <h6>- Meredakan rasa sakit</h6>
+            <h6>- Menjaga kesehatan saluran pencernaan</h6>
+            <h6>- Mengontrol kadar gula darah</h6>
+            <h6>- Menjaga kesehatan otak</h6>
+            <h6>- Menghambat pertumbuhan sel kanker</h6>
+          </div>
+        </div>    
+    """,
+
+    'Sirih': """
+        <div class="row gy-4">
+          <div class="col-lg-6 position-relative align-self-start order-lg-last order-first">
+            <img src="../static/img/sirih.jpg" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-6 content order-last  order-lg-first">
+            <h3>Daun Sirih</h3>
+            <p>
+               Daun sirih merah (Piper crocatum) biasanya dikonsumsi dengan cara diminum air rebusannya atau dikunyah langsung. Selain itu, ada juga suplemen yang dibuat dari ekstrak daun 
+               sirih merah. manfaat daun sirih merah berkat beragam senyawa yang terkandung di dalamnya, seperti flavonoid, fenol, alkaloid, polifenolat, tanin, saponin, dan senyawa minyak atsiri. 
+               Daun sirih juga dapat digunakan sebagai bahan pemutih kulit wajah. Selain itu, berikut adalah beragam manfaat daun sirih merah bagi kesehatan:
+            </p>
+            <h6>- Mempercepat penyembuhan luka</h6>
+            <h6>- Mencegah infeksi bakteri</h6>
+            <h6>- Mengobati rheumatoid arthritis</h6>
+            <h6>- Melawan radikal bebas</h6>
+            <h6>- Mengobati diabetes</h6>
+            <h6>- Mencegah pembentukan plak gigi</h6>
+          </div>
+        </div>    
+    """,
+
+    'Som_Jawa': """
+        <div class="row gy-4">
+          <div class="col-lg-6 position-relative align-self-start order-lg-last order-first">
+            <img src="../static/img/jawa.jpg" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-6 content order-last  order-lg-first">
+            <h3>Som Jawa</h3>
+            <p>
+               Som Jawa adalah salah satu tanaman herbal yang populer di Indonesia. Tanaman dengan nama latin Talinum paniculatum ini banyak ditemukan di Afrika dan Asia. Biasanya, Som Jawa kerap dijadikan 
+               tanaman hias dan digunakan untuk mengobati berbagai penyakit. Melansir dari Herbpathy, Som Jawa memiliki kandungan kandungan senyawa fitokimia berupa saponin, flavonoid, dan tannin. Selain itu, 
+               Som Jawa juga mengandung senyawa bersifat androgenik yang berperan penting untuk menjaga kesehatan tubuh. Berikut beberapa manfaat Som Jawa untuk kesehatan yang dilansir dari Herbpathy:
+            </p>
+            <h6>- Meningkatkan Vitalitas</h6>
+            <h6>- Mengatasi Sakit Kepala</h6>
+            <h6>- Mengatasi Sakit Maag</h6>
+            <h6>- Mencegah Penyakit Paru-Paru</h6>
+            <h6>- Melancarkan Haid</h6>
+            <h6>- Melancarkan ASI</h6>
+          </div>
+        </div>    
+    """
+
+}
